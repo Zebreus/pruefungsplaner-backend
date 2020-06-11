27fb96a771af1ce04d0a21ed274e4cd170c7cd15
Lennart Eichhorn <lennart@madmanfred.com>
2020-06-11 17:14:54 +0200

    Changed main to start the exam planner server

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <QCoreApplication>
+#include "qt-jsonrpc-server/src/jsonrpcserver.h"
+#include "src/examplannerserver.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    JsonRpcServer<ExamPlannerServer> examPlannerServer(56730);
+    examPlannerServer.startListening();
 
     return a.exec();
 }
